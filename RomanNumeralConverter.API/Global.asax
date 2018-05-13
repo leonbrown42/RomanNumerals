45a64c76c691c4600da681270a104a827ab8b4e8
leonbrown42 <leonbrown51@hotmail.com>
2018-05-13 12:31:27 +1200

    RomanNumeralConverter.API
    First batch because of 100 file limit

---
+++ RomanNumeralConverter.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RomanNumeralConverter.API.WebApiApplication" Language="C#" %>
---
+++ RomanNumeralConverter.API/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RomanNumeralConverter.API.Startup))]
+
+namespace RomanNumeralConverter.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
