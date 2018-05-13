3e9fbb2681d5fecd0f8901c4ad3504e861dd35ef
leonbrown42 <leonbrown51@hotmail.com>
2018-05-13 12:15:48 +1200

    Commit RomanNumeralConverter

---
+++ RomanNumeralConverter/ConvertBySubtraction.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RomanNumeralConverter
+{
+    public static class ConvertBySubtraction
+    {
+        private static Dictionary<int, char> RomanCharacters = new Dictionary<int, char>()
+        {
+            { 1, 'I' },
+            { 5, 'V' },
+            { 10, 'X' },
+            { 50, 'L' },
+            { 100, 'C' },
+            { 500, 'D' },
+            { 1000, 'M' }
+        };
+
+        public static string Convert(int number)
+        {
+            if (number >=4000)
+                throw new ArgumentException("Roman numerals have a maximum calculation value of 3999");
+
+            return Convert(ref number, RomanCharacters.Count - 1);
+        }
+
+        private static string Convert(ref int number, int index = 6, StringBuilder romanNumeralBuilder = null)
+        {
+            if (romanNumeralBuilder == null)
+                romanNumeralBuilder = new StringBuilder();
+
+            var characterValue = GetRomanCharacterValue(index);
+            var nextCharacter = GetNextCharacterValue(index);
+
+            if (CanCharacterBeAddedToRomanString(number, characterValue))
+                AddCharacterAndSubtractNumberValue(ref number, index, romanNumeralBuilder);
+
+            if (CanNextIndexedCharacterBeAddedToRomanString(number, characterValue, nextCharacter.Value))
+            {
+                if (IsNextIndexedCharacterAvailable(nextCharacter.Value))
+                    AddCharaterAndAddNumberValue(ref number, nextCharacter.Key, romanNumeralBuilder);
+
+                AddCharacterAndSubtractNumberValue(ref number, index, romanNumeralBuilder);
+            }
+
+            if (number != 0 && index > 0)
+                Convert(ref number, index - 1, romanNumeralBuilder);
+
+            return romanNumeralBuilder.ToString();
+        }
+
+        private static int GetRomanCharacterValue(int index)
+        {
+            return RomanCharacters.Keys.ElementAt(index);
+        }
+
+        private static char GetRomanCharacter(int index)
+        {
+            return RomanCharacters[GetRomanCharacterValue(index)];
+        }
+
+        private static KeyValuePair<Int16, int> GetNextCharacterValue(int index)
+        {
+            if (index < 1)
+                return new KeyValuePair<Int16, int>(); ;
+
+            var character = GetRomanCharacter(index - 1);
+            //Todo: ignorelist
+            if (character == 'V' || character == 'L' || character == 'D')
+                return new KeyValuePair<Int16, int>((Int16)(index - 2), GetRomanCharacterValue(index - 2));
+
+            return new KeyValuePair<Int16, int>((Int16)(index - 1), GetRomanCharacterValue(index - 1));
+        }
+
+        private static bool CanCharacterBeAddedToRomanString(int number, int characterValue)
+        {
+            return number >= characterValue;
+        }
+
+        private static void AddCharacterAndSubtractNumberValue(ref int number, int index, StringBuilder romanNumeralBuilder)
+        {
+            romanNumeralBuilder.Append(GetRomanCharacter(index));
+            number -= GetRomanCharacterValue(index);
+
+            if (number > 0)
+                Convert(ref number, index, romanNumeralBuilder);
+        }
+
+        private static void AddCharaterAndAddNumberValue(ref int number, int index, StringBuilder romanNumeralBuilder)
+        {
+            romanNumeralBuilder.Append(GetRomanCharacter(index));
+            number += GetRomanCharacterValue(index);
+        }
+
+        private static bool CanNextIndexedCharacterBeAddedToRomanString(int number, int characterValue, int nextCharacterValue)
+        {
+            return (number >= characterValue - nextCharacterValue);
+        }
+
+        private static bool IsNextIndexedCharacterAvailable(int nextCharacterValue)
+        {
+            return (nextCharacterValue > 0);
+        }
+    }
+}
---
+++ RomanNumeralConverter/Converter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RomanNumeralConverter
+{
+    public static class Converter
+    {
+        private static char One = 'I';
+        private static char Five = 'V';
+        private static char Ten = 'X';
+        private static char Fifty = 'L';
+        private static char Hundred = 'C';
+        private static char FiveHundred = 'D';
+        private static char Thousand = 'M';
+        private static Dictionary<int, char> RomanCharacters = new Dictionary<int, char>()
+        {
+            { 1, One },
+            { 5, Five },
+            { 10, Ten },
+            { 50, Fifty },
+            { 100, Hundred },
+            { 500, FiveHundred },
+            { 1000, Thousand }
+        };
+        private static List<int> RomanCharacterList = new List<int> { 1, 5, 10, 50, 100, 500, 1000 };
+
+        public static string Convert(int integer, int index = 6, StringBuilder romanNumeralBuilder = null)
+        {
+            //RomanOptions = new Dictionary<int, string> { { 1, "" } };
+            if (romanNumeralBuilder == null)
+                romanNumeralBuilder = new StringBuilder();
+
+            //if (integer > 1000) { }
+            //if (integer > 500) { }
+            //if (integer > 100) { }
+            //if (integer > 50) { }
+            //if (integer > 10) { }
+            var nextWritableRomanNumeral = RomanCharacterList[index] - (index == 0 ? 0 : RomanCharacterList[index - 1]);
+            var romanCharactersContainsNextNumeral = RomanCharacters.Any(x => x.Key == nextWritableRomanNumeral);
+            if (integer >= (nextWritableRomanNumeral) && !romanCharactersContainsNextNumeral)
+                AddBaseCharacter(ref integer, RomanCharacters[RomanCharacterList[index]], ref romanNumeralBuilder);
+
+            if (index > 0)
+                AddAdditionalCharacter(ref integer, RomanCharacters[RomanCharacterList[index - 1]], ref romanNumeralBuilder);
+
+            if (integer > 0)
+                Convert(integer, index - 1, romanNumeralBuilder);
+
+            if (integer < 0)
+                throw new OverflowException($"{nameof(integer)} should be 0 after the conversion, current value is {integer}");
+
+            return romanNumeralBuilder.ToString();
+        }
+
+        private static void AddBaseCharacter(ref int integer, char character, ref StringBuilder romanNumeralBuilder)
+        {
+            romanNumeralBuilder.Append(character);
+            var value = RomanCharacters.FirstOrDefault(x => x.Value == character).Key;
+            integer = integer - value;
+        }
+
+        private static void AddAdditionalCharacter(ref int integer, char character, ref StringBuilder romanNumeralBuilder)
+        {
+            //if (string.IsNullOrWhiteSpace(romanNumeralBuilder.ToString()) || integer == 0)
+            //    return;
+
+            var value = RomanCharacters.FirstOrDefault(x => x.Value == character).Key;
+
+            while (integer > 0 && (integer == value || integer == (value * 2) || integer == (value * 3)))
+            {
+                romanNumeralBuilder.Append(character);
+                integer = integer - value;
+            }
+
+            if (integer == -value)
+            {
+                romanNumeralBuilder.Insert(0, character);
+                integer = integer + value;
+            }
+        }
+    }
+}
